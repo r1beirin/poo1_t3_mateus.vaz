622701600c62bc47ccbcac7bf3ed4ca3ba4a7b2b
Mateus Ribeiro Vaz Pereira <12111bsi255@facom.local>
2022-10-03 20:27:48 -0300

    ex 01 resolv

---
+++ LAB01 - Procedural/ex01/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld{
+	public static void main(String[] args){
+		System.out.println("Hello World");
+		System.out.println("Mateus Ribeiro Vaz Pereira");
+		System.out.println("12111BSI255");
+	}
+}
