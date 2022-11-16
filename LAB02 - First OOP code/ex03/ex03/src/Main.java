f248ce816ddb972f229a04f42384677ff8289d80
r1beirin <mateusriibeiro@gmail.com>
2022-11-16 12:06:26 -0300

    usando scanner em setters

--- LAB02 - First OOP code/ex03/ex03/src/Main.java
+++ LAB02 - First OOP code/ex03/ex03/src/Main.java
@@ -1,44 +1,39 @@
 import java.sql.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Figurinhas[] jogador = new Figurinhas[3];
 
         for(int i = 0; i < 3; i++){
             jogador[i] = new Figurinhas();
         }
 
-        //  Jogador 1
-        jogador[0].setName("Luva de pedreiro");
-        jogador[0].setBirthday(Date.valueOf("1666-04-20"));
-        jogador[0].setHeight(2);
-        jogador[0].setWeight(90);
-        jogador[0].setPosition("tacante");
-        jogador[0].setCountry("Brazil");
-
-        //  Jogador 2
-        jogador[1].setName("Luva de marceneiro");
-        jogador[1].setBirthday(Date.valueOf("1666-04-21"));
-        jogador[1].setHeight(1.5F);
-        jogador[1].setWeight(85);
-        jogador[1].setPosition("efesa");
-        jogador[1].setCountry("Brazil");
-
-        //  Jogador 3
-        jogador[2].setName("Luva de motorista");
-        jogador[2].setBirthday(Date.valueOf("1666-04-23"));
-        jogador[2].setHeight(1.8F);
-        jogador[2].setWeight(86);
-        jogador[2].setPosition("agueiro");
-        jogador[2].setCountry("Brazil");
+        for(int i = 0; i < 3; i++){
+            System.out.printf("Digite o nome do jogador %d: ", i+1);
+            jogador[i].setName(sc.nextLine());
+            System.out.printf("Digite a data de nascimento (ano-mes-dia) do jogador %d: ", i+1);
+            jogador[i].setBirthday(Date.valueOf(sc.nextLine()));
+            System.out.printf("Digite a altura do jogador %d: ", i+1);
+            jogador[i].setHeight(sc.nextFloat());
+            System.out.printf("Digite o peso do jogador %d: ", i+1);
+            jogador[i].setWeight(sc.nextFloat());
+            sc.nextLine();
+            System.out.printf("Digite a posição do jogador %d: ", i+1);
+            jogador[i].setPosition(sc.nextLine());
+            System.out.printf("Digite o pais do jogador %d: ", i+1);
+            jogador[i].setCountry(sc.nextLine());
+            System.out.println();
+        }
 
         for(int i = 0; i < 3; i++){
-            System.out.println(jogador[i].getName());
-            System.out.println(jogador[i].getBirthday());
-            System.out.println(jogador[i].getHeight());
-            System.out.println(jogador[i].getWeight());
-            System.out.println(jogador[i].getPosition());
-            System.out.println(jogador[i].getCountry());
+            System.out.printf("Nome do jogador %d: %s\n", i+1, jogador[i].getName());
+            System.out.printf("Data de nascimento do jogador %d: %s\n", i+1, jogador[i].getBirthday());
+            System.out.printf("Altura do jogador %d: %.2f\n", i+1, jogador[i].getHeight());
+            System.out.printf("Peso do jogador %d: %.2f\n", i+1, jogador[i].getWeight());
+            System.out.printf("Posição do jogador %d: %s\n", i+1, jogador[i].getPosition());
+            System.out.printf("País do jogador %d: %s\n", i+1, jogador[i].getCountry());
             if(i == 2) continue;
             else System.out.println("========================================");
         }
--- LAB02 - First OOP code/ex04/ex04/src/Main.java
+++ LAB02 - First OOP code/ex04/ex04/src/Main.java
@@ -1,7 +1,9 @@
 import java.sql.Date;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
         Figurinhas[] jogadorSel1 = new Figurinhas[11];
         Figurinhas[] jogadorSel2 = new Figurinhas[11];
         Selecao selecao1 = new Selecao();
@@ -13,169 +15,49 @@ public class Main {
         }
 
         //  Instanciação jogadores seleção 1
-        jogadorSel1[0].setName("Luva de pedreiro");
-        jogadorSel1[0].setBirthday(Date.valueOf("1666-04-20"));
-        jogadorSel1[0].setHeight(2);
-        jogadorSel1[0].setWeight(90);
-        jogadorSel1[0].setPosition("tacante");
-        jogadorSel1[0].setCountry("Brazil");
-
-        jogadorSel1[1].setName("Luva de marceneiro");
-        jogadorSel1[1].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[1].setHeight(1.5F);
-        jogadorSel1[1].setWeight(85);
-        jogadorSel1[1].setPosition("efesa");
-        jogadorSel1[1].setCountry("Brazil");
-
-        jogadorSel1[2].setName("Luva de padeiro");
-        jogadorSel1[2].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[2].setHeight(1.5F);
-        jogadorSel1[2].setWeight(85);
-        jogadorSel1[2].setPosition("efesa");
-        jogadorSel1[2].setCountry("Brazil");
-
-        jogadorSel1[3].setName("Luva de calcario");
-        jogadorSel1[3].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[3].setHeight(1.5F);
-        jogadorSel1[3].setWeight(85);
-        jogadorSel1[3].setPosition("efesa");
-        jogadorSel1[3].setCountry("Brazil");
-
-        jogadorSel1[4].setName("Luva de boxe");
-        jogadorSel1[4].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[4].setHeight(1.5F);
-        jogadorSel1[4].setWeight(85);
-        jogadorSel1[4].setPosition("efesa");
-        jogadorSel1[4].setCountry("Brazil");
-
-        jogadorSel1[5].setName("Luva de vendedor");
-        jogadorSel1[5].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[5].setHeight(1.5F);
-        jogadorSel1[5].setWeight(85);
-        jogadorSel1[5].setPosition("efesa");
-        jogadorSel1[5].setCountry("Brazil");
-
-        jogadorSel1[6].setName("Luva de goleiro");
-        jogadorSel1[6].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[6].setHeight(1.5F);
-        jogadorSel1[6].setWeight(85);
-        jogadorSel1[6].setPosition("efesa");
-        jogadorSel1[6].setCountry("Brazil");
-
-        jogadorSel1[7].setName("Luva de otaco");
-        jogadorSel1[7].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[7].setHeight(1.5F);
-        jogadorSel1[7].setWeight(85);
-        jogadorSel1[7].setPosition("efesa");
-        jogadorSel1[7].setCountry("Brazil");
-
-        jogadorSel1[8].setName("Luva de ladrao");
-        jogadorSel1[8].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[8].setHeight(1.5F);
-        jogadorSel1[8].setWeight(85);
-        jogadorSel1[8].setPosition("efesa");
-        jogadorSel1[8].setCountry("Brazil");
-
-        jogadorSel1[9].setName("Luva de lixeiro");
-        jogadorSel1[9].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[9].setHeight(1.5F);
-        jogadorSel1[9].setWeight(85);
-        jogadorSel1[9].setPosition("efesa");
-        jogadorSel1[9].setCountry("Brazil");
-
-        jogadorSel1[10].setName("Luva de computeiro");
-        jogadorSel1[10].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel1[10].setHeight(1.5F);
-        jogadorSel1[10].setWeight(85);
-        jogadorSel1[10].setPosition("efesa");
-        jogadorSel1[10].setCountry("Brazil");
+        System.out.println("========== Jogadores da seleção 1 ====================");
+        for(int i = 0; i < 11; i++){
+            System.out.printf("Digite o nome do jogador %d: ", i+1);
+            jogadorSel1[i].setName(sc.nextLine());
+            System.out.printf("Digite a data de nascimento (ano-mes-dia) do jogador %d: ", i+1);
+            jogadorSel1[i].setBirthday(Date.valueOf(sc.nextLine()));
+            System.out.printf("Digite a altura do jogador %d: ", i+1);
+            jogadorSel1[i].setHeight(sc.nextFloat());
+            System.out.printf("Digite o peso do jogador %d: ", i+1);
+            jogadorSel1[i].setWeight(sc.nextFloat());
+            sc.nextLine();
+            System.out.printf("Digite a posição do jogador %d: ", i+1);
+            jogadorSel1[i].setPosition(sc.nextLine());
+            System.out.printf("Digite o pais do jogador %d: ", i+1);
+            jogadorSel1[i].setCountry(sc.nextLine());
+            System.out.println();
+        }
 
         selecao1.setName("Brazil");
         selecao1.setTecnico("Luxemburgo");
         selecao1.setJogador(jogadorSel1);
 
         //  Instanciação jogadores seleção 2
-        jogadorSel2[0].setName("Luva de pedreiro");
-        jogadorSel2[0].setBirthday(Date.valueOf("1666-04-20"));
-        jogadorSel2[0].setHeight(2);
-        jogadorSel2[0].setWeight(90);
-        jogadorSel2[0].setPosition("tacante");
-        jogadorSel2[0].setCountry("Mexico");
-
-        jogadorSel2[1].setName("Luva de marceneiro");
-        jogadorSel2[1].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[1].setHeight(1.5F);
-        jogadorSel2[1].setWeight(85);
-        jogadorSel2[1].setPosition("efesa");
-        jogadorSel2[1].setCountry("Mexico");
-
-        jogadorSel2[2].setName("Luva de padeiro");
-        jogadorSel2[2].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[2].setHeight(1.5F);
-        jogadorSel2[2].setWeight(85);
-        jogadorSel2[2].setPosition("efesa");
-        jogadorSel2[2].setCountry("Mexico");
-
-        jogadorSel2[3].setName("Luva de calcario");
-        jogadorSel2[3].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[3].setHeight(1.5F);
-        jogadorSel2[3].setWeight(85);
-        jogadorSel2[3].setPosition("efesa");
-        jogadorSel2[3].setCountry("Mexico");
-
-        jogadorSel2[4].setName("Luva de boxe");
-        jogadorSel2[4].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[4].setHeight(1.5F);
-        jogadorSel2[4].setWeight(85);
-        jogadorSel2[4].setPosition("efesa");
-        jogadorSel2[4].setCountry("Mexico");
-
-        jogadorSel2[5].setName("Luva de vendedor");
-        jogadorSel2[5].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[5].setHeight(1.5F);
-        jogadorSel2[5].setWeight(85);
-        jogadorSel2[5].setPosition("efesa");
-        jogadorSel2[5].setCountry("Mexico");
-
-        jogadorSel2[6].setName("Luva de goleiro");
-        jogadorSel2[6].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[6].setHeight(1.5F);
-        jogadorSel2[6].setWeight(85);
-        jogadorSel2[6].setPosition("efesa");
-        jogadorSel2[6].setCountry("Mexico");
-
-        jogadorSel2[7].setName("Luva de otaco");
-        jogadorSel2[7].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[7].setHeight(1.5F);
-        jogadorSel2[7].setWeight(85);
-        jogadorSel2[7].setPosition("efesa");
-        jogadorSel2[7].setCountry("Mexico");
-
-        jogadorSel2[8].setName("Luva de ladrao");
-        jogadorSel2[8].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[8].setHeight(1.5F);
-        jogadorSel2[8].setWeight(85);
-        jogadorSel2[8].setPosition("efesa");
-        jogadorSel2[8].setCountry("Mexico");
-
-        jogadorSel2[9].setName("Luva de lixeiro");
-        jogadorSel2[9].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[9].setHeight(1.5F);
-        jogadorSel2[9].setWeight(85);
-        jogadorSel2[9].setPosition("efesa");
-        jogadorSel2[9].setCountry("Mexico");
-
-        jogadorSel2[10].setName("Luva de computeiro");
-        jogadorSel2[10].setBirthday(Date.valueOf("1666-04-21"));
-        jogadorSel2[10].setHeight(1.5F);
-        jogadorSel2[10].setWeight(85);
-        jogadorSel2[10].setPosition("efesa");
-        jogadorSel2[10].setCountry("Mexico");
+        System.out.println("========== Jogadores da seleção 2 ====================");
+        for(int i = 0; i < 11; i++){
+            System.out.printf("Digite o nome do jogador %d: ", i+1);
+            jogadorSel2[i].setName(sc.nextLine());
+            System.out.printf("Digite a data de nascimento (ano-mes-dia) do jogador %d: ", i+1);
+            jogadorSel2[i].setBirthday(Date.valueOf(sc.nextLine()));
+            System.out.printf("Digite a altura do jogador %d: ", i+1);
+            jogadorSel2[i].setHeight(sc.nextFloat());
+            System.out.printf("Digite o peso do jogador %d: ", i+1);
+            jogadorSel2[i].setWeight(sc.nextFloat());
+            sc.nextLine();
+            System.out.printf("Digite a posição do jogador %d: ", i+1);
+            jogadorSel2[i].setPosition(sc.nextLine());
+            System.out.printf("Digite o pais do jogador %d: ", i+1);
+            jogadorSel2[i].setCountry(sc.nextLine());
+            System.out.println();
+        }
 
         selecao2.setName("Mexico");
         selecao2.setTecnico("Pelé");
         selecao2.setJogador(jogadorSel2);
-
-
     }
 }
