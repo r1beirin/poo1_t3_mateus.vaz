ad61bb17a6ad6f9e08be51b14b5cc1ad50c5997d
r1beirin <mateusriibeiro@gmail.com>
2022-12-12 17:29:04 -0300

    ex 01 lab classe abstrata

---
+++ LAB05 - Abstract classes/ex01/ex01/src/ContaCorrente.java
@@ -0,0 +1,54 @@
+public class ContaCorrente {
+    private float saldo;
+    private int estado; //  1 = conta ativa, 2 = conta inativa
+    private int numConta;
+    private int senha;
+
+    public ContaCorrente(float val, int num, int pwd) {
+        numConta = num;
+        senha = pwd;
+        saldo = val;
+        estado = 1; //  Conta ativa
+    }
+
+    public boolean debitaValor(float val, int pwd){
+        /*
+            Senha deve ser válida
+            Conta deve ser ativa
+            Val > 0
+         */
+        if(pwd != senha || estado != 1 || val <= 0 || val > saldo) return false;
+
+        saldo -= val;
+
+        if(saldo == 0) estado = 2;  // Conta torna inativa
+        return true;
+    }
+
+    public void debitaValor(float val){
+        saldo -= val;
+    }
+
+    public float getSaldo(int pwd){
+        if(senha == pwd) return saldo;
+        else return -1; //  Houve problema na senha
+    }
+
+    public void creditaValor(int pwd, float val){
+        if(senha == pwd) saldo += val;
+    }
+
+    protected int getEstado(int pwd){
+        if(senha == pwd) return estado;
+        else return -1;
+    }
+
+    protected void setEstado(int pwd, int e){
+        if(senha == pwd) estado = e;
+    }
+
+    protected boolean isSenha(int pwd){
+        if(senha == pwd) return true;
+        else return false;
+    }
+}
